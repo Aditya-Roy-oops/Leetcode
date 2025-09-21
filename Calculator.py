83704577d8020995e3ac3006958fb91db4ab1308
Aditya-Roy-oops <asrpcp11@gmail.com>
2025-09-21 18:19:49 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+#Calculator 
+r = eval(input("enter expression: "))
+print("result:", r)
+
+# it evaluates the expression and prints the result
+# for example, if the input is "3 + 4", it will output "result: 7"
+# if the input is "5 * 6", it will output "result: 30"
